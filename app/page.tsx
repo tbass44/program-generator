5175958aa14910691dc06ee98650deaa1c7b6bf5
tbass44 <ts0815@live.jp>
2026-05-06 15:22:50 +0900

    app/page.tsxからリダイレクト挙動追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <div
-      style={{
-        maxWidth: 1280,
-        margin: '0 auto',
-        padding: '2rem',
-        textAlign: 'center',
-      }}
-    >
-      Start prompting.
-    </div>
-  );
+  redirect('/admin/dashboard');
 }
